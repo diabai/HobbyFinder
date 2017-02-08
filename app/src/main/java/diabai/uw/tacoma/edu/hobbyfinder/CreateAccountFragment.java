1724b559c29fa94e9c6c3ae083b2948e838de881
Ibrahim Diabate <diabai@uw.edu>
2017-02-08 13:26:40 -0800

    wed

--- app/src/main/java/diabai/uw/tacoma/edu/hobbyfinder/CreateAccountFragment.java
+++ app/src/main/java/diabai/uw/tacoma/edu/hobbyfinder/CreateAccountFragment.java
@@ -11,9 +11,9 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-import com.google.android.gms.plus.PlusOneButton;
-
-
+/**
+ *
+ */
 public class CreateAccountFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";

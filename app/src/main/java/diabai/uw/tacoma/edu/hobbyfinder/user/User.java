73d112b15cd55fb1c5523222b378e7be50ee8144
Edgard Solorzano <edgards@uw.edu>
2017-02-09 15:54:58 -0800

    delete-unused imports

--- app/src/main/java/diabai/uw/tacoma/edu/hobbyfinder/user/User.java
+++ app/src/main/java/diabai/uw/tacoma/edu/hobbyfinder/user/User.java
@@ -1,10 +1,6 @@
 package diabai.uw.tacoma.edu.hobbyfinder.user;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 
 public class User implements Serializable {

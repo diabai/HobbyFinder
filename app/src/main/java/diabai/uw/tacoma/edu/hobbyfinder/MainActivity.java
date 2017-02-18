331049e83f133c60c827e357a5805ba46df49862
Ibrahim Diabate <diabai@uw.edu>
2017-02-17 22:58:16 -0800

    Save data from selected hobbies and send back to create account frag

--- app/src/main/java/diabai/uw/tacoma/edu/hobbyfinder/MainActivity.java
+++ app/src/main/java/diabai/uw/tacoma/edu/hobbyfinder/MainActivity.java
@@ -17,6 +17,9 @@ import diabai.uw.tacoma.edu.hobbyfinder.user.User;
 
 /**
  * Main activity class for the application.
+ *
+ * @Author: Ibrahim Diabate
+ * @version: 2.0
  */
 public class MainActivity extends AppCompatActivity implements
         LogInFragment.OnListFragmentInteractionListener,
